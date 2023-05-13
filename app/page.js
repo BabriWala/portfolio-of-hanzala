47962d25a21fa3f90b24de5a3b4f90eb0b9db2b7
BabriWala <hanzalafarhann@gmail.com>
2023-05-13 07:17:55 +0600

    review and testimonial section added

--- app/page.js
+++ app/page.js
@@ -2,12 +2,16 @@ import Image from 'next/image'
 import Hero from "../components/Hero/Hero"
 import Contact from '../components/Contact/Contact'
 import Footer from "../components/Footer/Footer"
+import Review from '@/components/Review/Review'
+import Projects from '@/components/Projects/Projects'
 
 
 export default function Home() {
   return (
       <header className=" mr-16 mb-80px">
        <Hero></Hero>
+       <Projects></Projects>
+       <Review></Review>
        <Contact></Contact>
        <Footer></Footer>
       </header>
--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,10 +1,48 @@
+"use client"
 import React from 'react'
+import { useState, useEffect } from 'react';
+import parse from 'html-react-parser';
 
 const Hero = () =>{
-    return (
-    <div className="h-screen bg-[#13CE66]">
 
-    </div>
+    const TYPING_SPEED = 150;
+    const DELETING_SPEED = 30;
+
+    const [text, setText] = useState('');
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [typingSpeed, setTypingSpeed] = useState(TYPING_SPEED);
+    
+    useEffect(() => {
+        handleType();
+      }, []);
+    
+      const handleType = () => {
+        const fullText = "Hello World";
+    
+        setText((prevState) =>
+          isDeleting
+            ? fullText.substring(0, prevState.length - 1)
+            : fullText.substring(0, prevState.length + 1)
+        );
+
+        setTypingSpeed(isDeleting ? DELETING_SPEED : TYPING_SPEED);
+
+        if (!isDeleting && text === fullText) {
+            console.log("inside")
+          setTimeout(() => setIsDeleting(true), 500);
+        } else if (isDeleting && text === '') {
+          setIsDeleting(false);
+        }
+    
+        setTimeout(handleType, typingSpeed);
+      };
+    
+
+    const message = '<p className="m-0">Hellow world</p><p>Hellow world</p>'
+    return (
+        <div className="h-screen bg-[#13CE66]">
+            {text}
+        </div>
     )
 }
 
